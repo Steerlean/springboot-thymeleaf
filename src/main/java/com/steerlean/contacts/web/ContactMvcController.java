cb2ef26d2c412f0783f1472554fb13aafa548d12
JayeshKsh <jayesh.kshatriya12@gmail.com>
2019-07-05 11:32:21 +0530

    add login screen and logout button also

--- src/main/java/com/steerlean/contacts/web/ContactMvcController.java
+++ src/main/java/com/steerlean/contacts/web/ContactMvcController.java
@@ -2,10 +2,12 @@ package com.steerlean.contacts.web;
 
 import com.steerlean.contacts.exception.RecordNotFoundException;
 import com.steerlean.contacts.model.ContactEntity;
+import com.steerlean.contacts.model.UserEntity;
 import com.steerlean.contacts.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,7 +18,24 @@ import java.util.Optional;
 @Controller
 @RequestMapping("/")
 public class ContactMvcController {
-    @Autowired ContactService service;
+    @Autowired
+    ContactService service;
+
+    @RequestMapping(path = "/login")
+    public String getLoginForm() {
+        return "login";
+    }
+
+    @RequestMapping(path = "/login", method = RequestMethod.POST)
+    public String login(@ModelAttribute(name = "loginForm") UserEntity userEntity, Model model) {
+        String username = userEntity.getUsername();
+        String password = userEntity.getPassword();
+        if ("admin".equals(username) && "admin".equals(password)) {
+            return "list-contacts";
+        }
+        model.addAttribute("invalidCredentials", true);
+        return "login";
+    }
 
     @RequestMapping
     public String getAllContacts(Model model) {
@@ -26,7 +45,7 @@ public class ContactMvcController {
         return "list-contacts";
     }
 
-    @RequestMapping(path = { "/edit", "/edit/{id}" })
+    @RequestMapping(path = {"/edit", "/edit/{id}"})
     public String editContactsById(Model model, @PathVariable("id") Optional<Long> id) throws RecordNotFoundException {
         if (id.isPresent()) {
             ContactEntity entity = service.getContactById(id.get());
