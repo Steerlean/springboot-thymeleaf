f283e984bad50f884274e0ff03efd6c245f4ba6f
JayeshKsh <jayesh.kshatriya12@gmail.com>
2019-07-05 14:23:25 +0530

    added session functionality

--- src/main/java/com/steerlean/contacts/web/ContactMvcController.java
+++ src/main/java/com/steerlean/contacts/web/ContactMvcController.java
@@ -5,6 +5,7 @@ import com.steerlean.contacts.model.ContactEntity;
 import com.steerlean.contacts.model.UserEntity;
 import com.steerlean.contacts.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,23 +24,32 @@ public class ContactMvcController {
     @Autowired
     ContactService service;
 
-    @RequestMapping(path = "/login")
+    @RequestMapping(path = "/logout")
+    public String logOut(HttpServletRequest request) {
+        HttpSession httpSession = request.getSession();
+        httpSession.removeAttribute("username");
+        httpSession.invalidate();
+        return "login";
+    }
+
+    @RequestMapping(path = "/")
     public String getLoginForm() {
         return "login";
     }
 
-    @RequestMapping(path = "/login", method = RequestMethod.POST)
-    public String login(@ModelAttribute(name = "loginForm") UserEntity userEntity, Model model) {
+    @RequestMapping(path = "/", method = RequestMethod.POST)
+    public String login(@ModelAttribute(name = "loginForm") UserEntity userEntity, Model model, HttpSession httpSession) {
         String username = userEntity.getUsername();
         String password = userEntity.getPassword();
         if ("admin".equals(username) && "admin".equals(password)) {
+            httpSession.setAttribute("username", userEntity.getUsername());
             return "list-contacts";
         }
         model.addAttribute("invalidCredentials", true);
         return "login";
     }
 
-    @RequestMapping
+    @RequestMapping(path = "/getAllContacts")
     public String getAllContacts(Model model) {
         List<ContactEntity> list = service.getAllContacts();
 
@@ -59,12 +71,12 @@ public class ContactMvcController {
     @RequestMapping(path = "/delete/{id}")
     public String deleteContactsById(Model model, @PathVariable("id") Long id) throws RecordNotFoundException {
         service.deleteContactById(id);
-        return "redirect:/";
+        return "redirect:/getAllContacts";
     }
 
     @RequestMapping(path = "/createContact", method = RequestMethod.POST)
     public String createOrUpdateContact(ContactEntity contact) {
         service.createContact(contact);
-        return "redirect:/";
+        return "redirect:/getAllContacts";
     }
 }
